96849762015b388eefea480638ad1da2c793c3cd
KonstantyBialek <konstanty.bialek@student.wsb.edu.pl>
2023-06-04 12:29:41 +0200

    Initial functionality setup and basic UI layout added.

--- app/src/main/java/com/example/parkmanager/MainActivity.kt
+++ app/src/main/java/com/example/parkmanager/MainActivity.kt
@@ -3,44 +3,175 @@ package com.example.parkmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.parkmanager.ui.theme.ParkManagerTheme
+import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
-        setContent {
-            ParkManagerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContent { MainMenu() }
     }
 }
 
+/**
+ * Main menu view of the app.
+ */
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainMenu() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Placeholder Main Menu Screen", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ParkManagerTheme {
-        Greeting("Android")
+fun MainMenuPreview() {
+    MainMenu()
+}
+
+/**
+ * User login screen.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(onLoginSuccess: () -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login Screen", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { performLogin(email, password, onLoginSuccess) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
     }
+}
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen {}
+}
+
+/**
+ * User registration screen.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterScreen(onRegistrationSuccess: () -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Registration Screen", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Confirm Password") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { performRegistration(email, password, onRegistrationSuccess) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Register")
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun RegisterScreenPreview() {
+    RegisterScreen {}
+}
+
+/**
+ * Administrator access screen.
+ */
+@Composable
+fun AdminAccessScreen() {
+}
+
+
+/**
+ * @@ Business logic block below. @@
+ */
+fun performRegistration(email: String, password: String, onRegistrationSuccess: () -> Unit) {
+    TODO("Not yet implemented")
+}
+
+fun performLogin(email: String, password: String, onLoginSuccess: () -> Unit) {
+    TODO("Not yet implemented")
 }
---
+++ app/src/test/java/com/example/parkmanager/UnitTests.kt
@@ -0,0 +1,88 @@
+package com.example.parkmanager
+
+import org.junit.Test
+
+/**
+  * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class UnitTests {
+    /**
+     * User account management tests.
+     */
+    @Test
+    fun userAccount_isCreatedCorrectly() {
+        TODO()
+    }
+    @Test
+    fun userAccount_isDeletedCorrectly() {
+        TODO()
+    }
+    @Test
+    fun userAccount_isEditedCorrectly() {
+        TODO()
+    }
+
+    /**
+     * Parking lot admin-level management tests.
+     */
+    @Test
+    fun parkingSpace_isCreatedCorrectly() {
+        TODO()
+    }
+    @Test
+    fun parkingSpace_isDeletedCorrectly() {
+        TODO()
+    }
+    @Test
+    fun parkingSpace_isEditedCorrectly() {
+        TODO()
+    }
+
+    /**
+     * Parking space booking tests.
+     */
+    @Test
+    fun booking_isBookedCorrectly() {
+        TODO()
+    }
+    @Test
+    fun booking_isCancelledCorrectly() {
+        TODO()
+    }
+    @Test
+    fun booking_isEditedCorrectly() {
+        TODO()
+    }
+    @Test
+    fun booking_isDisplayedCorrectly() {
+        TODO()
+    }
+    @Test
+    fun booking_validatesUserInputCorrectly() {
+        TODO()
+    }
+    @Test
+    fun booking_displaysErrorsAtInvalidInputs() {
+        TODO()
+    }
+    @Test
+    fun booking_preventsConflictingBookings() {
+        TODO()
+    }
+    @Test
+    fun booking_updatesAfterBookingStatusChange() {
+        TODO()
+    }
+
+    /**
+     * Notifications tests.
+     */
+    @Test
+    fun notification_isDisplayedWhenBookingEnds() {
+        TODO()
+    }
+    @Test
+    fun notification_isDisplayedWhenBookingRequestDenied() {
+        TODO()
+    }
+}
